3730dc1b0654e1fd880b4b64484d2eb5b372b441
Your Name <you@example.com>
2024-06-03 18:07:07 +0300

    ddd

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script setup lang="ts">
-import Navbar from "./components/UI/Navbar.vue";
-import Footer from "./components/UI/Footer.vue";
+import Navbar from "@/components/UI/Navbar.vue";
+import Footer from "@/components/UI/Footer.vue";
 
 import { useStore } from "vuex";
 import { computed } from "vue";
